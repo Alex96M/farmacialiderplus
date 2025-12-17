1a60b1bc7e0c08a981045aebe902af324d934b19
Alex96M <87329406+Alex96M@users.noreply.github.com>
2025-12-17 12:17:08 -0600

    Update admin.js

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,46 +1,10 @@
+console.log("admin.js cargado");
+
 const token = localStorage.getItem("adminToken");
 if (!token) location.href = "login.html";
 
-let editId = null;
-
-async function cargar() {
-  const res = await fetch("/api/admin/productos", {
-    headers: { "x-admin-token": token }
-  });
-  const data = await res.json();
-
-  const ul = document.getElementById("lista");
-  ul.innerHTML = "";
-
-  data.forEach(p => {
-    ul.innerHTML += `
-      <li>
-        ${p.name} - $${p.price}
-        <button onclick="editar(${p.id})">Editar</button>
-        <button onclick="eliminar(${p.id})">Eliminar</button>
-      </li>
-    `;
-  });
-}
-
-async function subirImagen() {
-  const file = document.getElementById("imagen").files[0];
-  if (!file) return null;
-
-  const form = new FormData();
-  form.append("imagen", file);
-
-  const res = await fetch("/api/admin/upload", {
-    method: "POST",
-    headers: { "x-admin-token": token },
-    body: form
-  });
-
-  return (await res.json()).image;
-}
-
 async function guardar() {
-  const image = await subirImagen(); // puede ser null
+  console.log("Guardar clic");
 
   const producto = {
     name: document.getElementById("name").value.trim(),
@@ -48,7 +12,7 @@ async function guardar() {
     price: document.getElementById("price").value,
     category: document.getElementById("category").value.trim(),
     stock: document.getElementById("stock").value,
-    image: image || ""
+    image: ""
   };
 
   const res = await fetch("/api/admin/productos", {
@@ -65,53 +29,16 @@ async function guardar() {
     return;
   }
 
+  alert("Producto guardado correctamente");
   limpiar();
-  cargar();
-}
-
-
-  const method = editId ? "PUT" : "POST";
-  const url = editId
-    ? `/api/admin/productos/${editId}`
-    : "/api/admin/productos";
-
-  await fetch(url, {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      "x-admin-token": token
-    },
-    body: JSON.stringify(producto)
-  });
-
-  editId = null;
-  limpiar();
-  cargar();
-}
-
-function editar(id) {
-  editId = id;
-}
-
-async function eliminar(id) {
-  if (!confirm("¿Eliminar producto?")) return;
-
-  await fetch(`/api/admin/productos/${id}`, {
-    method: "DELETE",
-    headers: { "x-admin-token": token }
-  });
-
-  cargar();
 }
 
 function limpiar() {
-  name.value = "";
-  price.value = "";
-  stock.value = "";
-  category.value = "";
-  description.value = "";
-  imagen.value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("category").value = "";
+  document.getElementById("stock").value = "";
 }
 
-cargar();
 
