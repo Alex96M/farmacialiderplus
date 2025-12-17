ac6327ef64e02711774f6c42dc0a595c654edd0c
Alex96M <87329406+Alex96M@users.noreply.github.com>
2025-12-17 11:57:28 -0600

    Add files via upload

---
+++ public/catalogo.js
@@ -0,0 +1,18 @@
+fetch("/api/productos")
+  .then(res => res.json())
+  .then(productos => {
+    const contenedor = document.getElementById("catalogo");
+
+    productos.forEach(p => {
+      const div = document.createElement("div");
+      div.className = "producto";
+      div.innerHTML = `
+        <img src="/${p.image}">
+        <h3>${p.name}</h3>
+        <p>${p.description}</p>
+        <strong>$${p.price}</strong>
+      `;
+      contenedor.appendChild(div);
+    });
+  })
+  .catch(err => console.error(err));
